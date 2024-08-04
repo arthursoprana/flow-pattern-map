3f8420778b7955f1c5b793ceafac4ff6d46607e3
Arthur Soprana <arthur.soprana@cmgl.ca>
2024-08-04 12:11:01 -0300

    Update flowpat (FLOPATN) version and update notebooks
    .exe obtained from FLOWPATN 202.7.zip

--- README.md
+++ README.md
@@ -1,28 +1,13 @@
 # flow-pattern-map
 Jupyter Notebook with two-phase flow pattern map calculation.
 
-Please Contact to Dr. Ovadia Shoham by os@utulsa.edu or [Dr. Eduardo Pereyra](https://www.researchgate.net/profile/Eduardo_Pereyra2) for more info about `FlowPat.dll`.
+Please Contact to [Dr. Eduardo Pereyra](https://www.researchgate.net/profile/Eduardo_Pereyra2) for more info about `FlowPat.exe`.
 
 # Installation
 
-* Download [miniconda](https://conda.io/miniconda.html) (python 3.X - 64bit).
-
-        set CONDA_FORCE_32BIT=1
-        conda env create
-        activate py3_32bit
-
-# Before using the notebook
-
-## Disable Data Execution Prevention - DEP (Windows ONLY)
-
-1. Type cmd in the start search menu, **right-click** on it and select **Run as Administrator**.
-
-2. Once the command prompt is open, you can now disable [Data Execution Prevention](https://pt.wikipedia.org/wiki/Data_Execution_Prevention) (DEP) by entering the following command line.
-
-        bcdedit.exe /set nx AlwaysOff
-
-3. And don't forget to enable it back after using it (you may need to restart your machine),
-
-        bcdedit.exe /set nx AlwaysOn
-
-**NOTE**: Always remember to `set CONDA_FORCE_32BIT=1` before activating env.
+* Download [miniconda](https://conda.io/miniconda.html).
+* Call
+    ```
+    conda env create
+    conda activate flow-pattern-map
+    ```
--- buttons.py
+++ buttons.py
@@ -5,47 +5,47 @@ continuous_update = True
 layout = Layout(width='50%', justify_content ='space-between',  align_content='space-around')
 
 ρ_L_button = widgets.FloatSlider(
-    min=800.0, 
-    max=1200.0, 
-    value=1000.0, 
-    step=0.5, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    min=800.0,
+    max=1200.0,
+    value=1000.0,
+    step=0.5,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Liquid Density [kg/m3]',
     layout=layout
 )
 
 ρ_G_button = widgets.FloatSlider(
-    min=0.5, 
-    max=300.0, 
-    value=2.0, 
-    step=0.5, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    min=0.5,
+    max=300.0,
+    value=2.0,
+    step=0.5,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Gas Density [kg/m3]',
     layout=layout
 )
 
 # Viscosity
 μ_L_button = widgets.FloatSlider(
-    min=1e-3, 
-    max=1e-2, 
-    value=1e-3, 
-    step=1e-5, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    min=1e-3,
+    max=1e-2,
+    value=1e-3,
+    step=1e-5,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Liquid Viscosity [Pa.s]',
     readout_format='.2e',
     layout=layout
 )
 
 μ_G_button = widgets.FloatSlider(
-    min=1e-5, 
-    max=1e-3, 
-    value=1e-5, 
-    step=1e-6, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    min=1e-5,
+    max=1e-3,
+    value=1e-5,
+    step=1e-6,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Gas Viscosity [Pa.s]',
     readout_format='.2e',
     layout=layout
@@ -54,11 +54,11 @@ layout = Layout(width='50%', justify_content ='space-between',  align_content='s
 # Surface Tension
 σ_button = widgets.FloatSlider(
     min=0.01,
-    max=0.10, 
-    value=0.07, 
-    step=0.001, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    max=0.10,
+    value=0.07,
+    step=0.001,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Surface Tension [N/m]',
     layout=layout
 )
@@ -66,11 +66,11 @@ layout = Layout(width='50%', justify_content ='space-between',  align_content='s
 # Pipe Diameter
 D_button = widgets.FloatSlider(
     min=0.001,
-    max=0.60, 
-    value=0.1, 
-    step=0.001, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    max=0.60,
+    value=0.1,
+    step=0.001,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Pipe Diameter [m]',
     readout_format='.3f',
     layout=layout
@@ -79,22 +79,22 @@ D_button = widgets.FloatSlider(
 # Pipe Inclination
 θ_button = widgets.FloatSlider(
     min=0.0,
-    max=90.0, 
-    value=0.0, 
-    step=0.01, 
-    continuous_update=continuous_update, 
-    orientation='horizontal', 
+    max=90.0,
+    value=0.0,
+    step=0.5,
+    continuous_update=continuous_update,
+    orientation='horizontal',
     description='Pipe Inclination [deg]',
     layout=layout
 )
 
 # Pipe Roughness
 k_s_button = widgets.FloatSlider(
-    min=0.0, 
-    max=1e-3, 
+    min=0.0,
+    max=1e-3,
     value=1e-5,
-    step=1e-6, 
-    continuous_update=continuous_update, 
+    step=1e-6,
+    continuous_update=continuous_update,
     description='Pipe Roughness [m]',
     readout_format='.2e',
     layout=layout
@@ -107,10 +107,37 @@ interface_button = widgets.Dropdown(
     layout=layout
 )
 
+# Pipe Roughness
+k_s_button = widgets.FloatSlider(
+    min=0.0,
+    max=1e-3,
+    value=1e-5,
+    step=1e-6,
+    continuous_update=continuous_update,
+    description='Pipe Roughness [m]',
+    readout_format='.2e',
+    layout=layout
+)
 
-plot_mesh_button = widgets.RadioButtons(
-    options=['True', 'False'],
-    value='False',
-    description='Plot grid points:',
-    disabled=False
+# Superficial velocity
+us_G_button = widgets.FloatSlider(
+    min=1e-3,
+    max=100.0,
+    value=0.1,
+    step=1e-3,
+    continuous_update=continuous_update,
+    description='Gas Superficial Velocity [m/s]',
+    readout_format='.3f',
+    layout=layout
+)
+
+us_L_button = widgets.FloatSlider(
+    min=1e-3,
+    max=100.0,
+    value=0.01,
+    step=1e-3,
+    continuous_update=continuous_update,
+    description='Liquid Superficial Velocity [m/s]',
+    readout_format='.3f',
+    layout=layout
 )
